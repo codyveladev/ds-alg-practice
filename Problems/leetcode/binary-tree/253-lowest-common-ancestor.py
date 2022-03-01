24aebfd85a9506de1ec273ba6b61708d0bdcd487
Cody Vela <codyvela0702@gmail.com>
2022-02-28 18:07:53 -0600

    Day 20 - 02/28

--- Problems/leetcode/binary-tree/253-lowest-common-ancestor.py
+++ Problems/leetcode/binary-tree/253-lowest-common-ancestor.py
@@ -7,9 +7,12 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        #If both of the values are in the left subtree, then traverse left
         if p.val < root.val and q.val < root.val: 
             return self.lowestCommonAncestor(root.left, p, q)
+        #If both of the values are in the right subtree then traverse right
         elif p.val > root.val and q.val > root.val: 
             return self.lowestCommonAncestor(root.right, p, q)
+        #If it splits (one if left and one in right) this is the node to return 
         else:  
             return root
